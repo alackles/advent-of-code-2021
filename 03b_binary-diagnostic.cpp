12d6e75dde522d36a3a8d277dd35d94d271871d1
Acacia Ackles <alackles@msu.edu>
2021-12-04 10:19:02 -0500

    Rewrite most common bit task

--- 03b_binary-diagnostic.cpp
+++ 03b_binary-diagnostic.cpp
@@ -2,36 +2,49 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <numeric>
-#include <functional>
+#include <bitset>
 
-// parser function
-std::vector<std::string> col_parser(const std::string & fname) {
-  std::vector<std::string> vec;
-  std::string line;
+#define BITLEN 5
+
+
+// bitset parser function
+// I like reading things into memory before working with them
+// Sue me
+std::vector<std::bitset<BITLEN>> parse_bitset(const std::string & fname) {
+  
+  std::vector<std::bitset<BITLEN>> vec;
   std::ifstream datafile(fname);
+  std::string line;
+  
   while (getline(datafile, line)) {
-    vec.resize(line.size());
-    for (size_t i = 0; i < line.size(); ++i) {
-        vec[i].push_back(line[i]);
-    }   
+    std::bitset<BITLEN> bit_line(line);
+    vec.push_back(bit_line);
   }
+  
   return vec;
-}
+} 
 
 int main() {
-  std::vector<std::string> power = col_parser("data/03.txt");
-  size_t midpt = power[0].size()/2;
-  std::string gamma;
-  std::string epsilon;
-  for (auto col : power) {
-      size_t ct = std::count(col.begin(), col.end(), '1');
-      gamma.push_back((ct > midpt) ? '1' : '0');
-      epsilon.push_back((ct < midpt) ? '1' : '0');
+  std::vector<std::bitset<BITLEN>> power = parse_bitset("data/toy.txt");
+  
+  // find most common bit 
+  std::vector<int> ref_vec(BITLEN, 0);
+  for (auto line : power) {
+    for (int i = 0; i < BITLEN; i++) {
+      if (line[i] == 0) {
+        ref_vec[i]--;
+      } else {
+        ref_vec[i]++;
+      }
+    }
   }
-  int gamma_int = std::stoi(gamma, 0, 2);
-  int epsilon_int = std::stoi(epsilon, 0, 2);
-  std::cout << "Gamma: " << gamma_int << std::endl;
-  std::cout << "Epsilon: " << epsilon_int << std::endl;
-  std::cout << "Power: " << gamma_int * epsilon_int << std::endl;
+  
+  // convert that to 1s and 0s
+  std::bitset<BITLEN> gamma; 
+  for (size_t i = 0; i < BITLEN; i++) {
+    gamma[i] = (ref_vec[i] > 0) ? 1 : 0; 
+  }
+  std::bitset<BITLEN> epsilon = ~gamma;
+
+  std::cout << gamma.to_ulong() * epsilon.to_ulong() << std::endl;
 }
